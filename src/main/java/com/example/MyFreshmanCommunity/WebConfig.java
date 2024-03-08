3bcd955d9fe03932bef4a21ea973c741837c232c
chaewonkim <kimlove9182@gmail.com>
2024-03-08 21:23:29 +0900

    refactor: webConfig 수정

--- src/main/java/com/example/MyFreshmanCommunity/WebConfig.java
+++ src/main/java/com/example/MyFreshmanCommunity/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("*") //전체 다 받을 경우 Origin'Patterns"로 수정
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
